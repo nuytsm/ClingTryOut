d002b25a6487d55b4ffcd2a962f1b222b86bf8ec
nuyts.m <marknuyts@gmail.com>
2015-05-02 21:50:20 +0200

    identity detection for mediarender yamaha

--- YamahaRX-V475-spec/src/main/java/org/galdrtech/yamahRxV475spec/InfoRetriever.groovy
+++ YamahaRX-V475-spec/src/main/java/org/galdrtech/yamahRxV475spec/InfoRetriever.groovy
@@ -4,14 +4,21 @@ import org.fourthline.cling.UpnpService;
 import org.fourthline.cling.UpnpServiceImpl
 import org.fourthline.cling.binding.xml.Descriptor.Service;
 import org.fourthline.cling.model.message.header.STAllHeader;
+import org.fourthline.cling.model.message.header.UDADeviceTypeHeader
 import org.fourthline.cling.model.meta.LocalDevice;
 import org.fourthline.cling.model.meta.RemoteDevice;
+import org.fourthline.cling.model.types.UDADeviceType;
 import org.fourthline.cling.registry.Registry;
 import org.fourthline.cling.registry.RegistryListener;
 
 class InfoRetriever {
 
 	String ip = "10.0.0.15"
+	static String yamahaUuid = "5f9ec1b3-ed59-1900-4530-00a0"
+	
+	static boolean isYamahaMediaRendererDevice(RemoteDevice device){
+	 	return device.getIdentity().getUdn().getIdentifierString().startsWith(yamahaUuid)	
+	}
 
 	public static void main(String[] args) throws Exception {
 
@@ -20,7 +27,7 @@ class InfoRetriever {
 
 					public void remoteDeviceDiscoveryStarted(Registry registry,
 							RemoteDevice device) {
-						if (device.getDisplayString().contains("475")){
+						if (isYamahaMediaRendererDevice(device)){
 							System.out.println(
 									"Discovery started: " + device.getDisplayString()
 									);
@@ -30,7 +37,7 @@ class InfoRetriever {
 					public void remoteDeviceDiscoveryFailed(Registry registry,
 							RemoteDevice device,
 							Exception ex) {
-						if (device.getDisplayString().contains("475")){
+						if (isYamahaMediaRendererDevice(device)){
 							System.out.println(
 									"Discovery failed: " + device.getDisplayString() + " => " + ex
 									);
@@ -38,20 +45,27 @@ class InfoRetriever {
 					}
 
 					public void remoteDeviceAdded(Registry registry, RemoteDevice device) {
-						if (device.getDisplayString().contains("475")){
+						if (isYamahaMediaRendererDevice(device)){
 							System.out.println(
 									"Remote device available: " + device.getDisplayString()
 									);
+								println "Identity: " + device.getIdentity()
+							println "DETAILS: "
+							device.getDetails().getProperties().each {
+								println "${it.key} -> ${it.value}"
+							}
+							
 						}
+						
+						
+						
 					}
 
 					public void remoteDeviceUpdated(Registry registry, RemoteDevice device) {
-						if (device.getDisplayString().contains("475")){
+						if (isYamahaMediaRendererDevice(device)){
 							System.out.println(
 									"Remote device updated: " + device.getDisplayString()
 									);
-							println "DETAILS: "
-							println device.getDetails().getProperties()
 //							println "MetaPropValues: "
 //							Service s = device.findServices()[0]
 //							s?.
@@ -59,7 +73,7 @@ class InfoRetriever {
 					}
 
 					public void remoteDeviceRemoved(Registry registry, RemoteDevice device) {
-						if (device.getDisplayString().contains("475")){
+						if (isYamahaMediaRendererDevice(device)){
 							System.out.println(
 									"Remote device removed: " + device.getDisplayString()
 									);
@@ -67,7 +81,7 @@ class InfoRetriever {
 					}
 
 					public void localDeviceAdded(Registry registry, LocalDevice device) {
-						if (device.getDisplayString().contains("475")){
+						if (isYamahaMediaRendererDevice(device)){
 							System.out.println(
 									"Local device added: " + device.getDisplayString()
 									);
@@ -75,7 +89,7 @@ class InfoRetriever {
 					}
 
 					public void localDeviceRemoved(Registry registry, LocalDevice device) {
-						if (device.getDisplayString().contains("475")){
+						if (isYamahaMediaRendererDevice(device)){
 							System.out.println(
 									"Local device removed: " + device.getDisplayString()
 									);
@@ -100,7 +114,14 @@ class InfoRetriever {
 		UpnpService upnpService = new UpnpServiceImpl(listener);
 
 		// Send a search message to all devices and services, they should respond soon
-		upnpService.getControlPoint().search(new STAllHeader());
+//		upnpService.getControlPoint().search(new STAllHeader());
+		
+		//specific:
+		//ST: urn:schemas-upnp-org:device:MediaRenderer:1
+		UDADeviceType udaType = new UDADeviceType("MediaRenderer");
+			upnpService.getControlPoint().search(
+			        new UDADeviceTypeHeader(udaType)
+			);
 
 		// Let's wait 10 seconds for them to respond
 		System.out.println("Waiting 5 seconds before shutting down...");
